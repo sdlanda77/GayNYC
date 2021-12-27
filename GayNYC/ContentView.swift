80ff812d5edd39a8d9ec006cc31bfb38e052d6de
Sam L <sdlanda77@gmail.com>
2021-12-27 09:25:58 -0500

    Initial Commit

---
+++ GayNYC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GayNYC
+//
+//  Created by Sam Landa on 12/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GayNYC/GayNYCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GayNYCApp.swift
+//  GayNYC
+//
+//  Created by Sam Landa on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct GayNYCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
